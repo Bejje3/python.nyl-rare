015d0d38ad116d0fbcdaa0a7a7d8ce8f5db4287a
Bejje3 <128036765+Bejje3@users.noreply.github.com>
2023-11-07 11:32:30 +0100

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,26 @@
+"""
+namn = "Benjamin"  
+nummer = 4 
+
+nummer2 = 8 
+
+nummer3 = nummer + nummer2 
+
+print("skriv ett nummer")  
+
+nummer = input() 
+nummer2 = input() 
+
+print(int(nummer) + int(nummer2))   
+""" 
+
+nummer = int(input()) 
+
+if nummer < 10: 
+    print("numret är mindre än 10!") 
+
+if nummer == 10: 
+    print("numret är 10!")  
+
+elif nummer > 10: 
+    print("numret är större än 10!")  
