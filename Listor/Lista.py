28b46ec17939d71f7022ab52db7f19c046fafe6e
Bejje3 <128036765+Bejje3@users.noreply.github.com>
2024-02-06 11:24:59 +0100

    Add files via upload

---
+++ Listor/Lista.py
@@ -0,0 +1,25 @@
+"""
+list1 = ["He", "å", "di"]
+list2 = ["j", "P" "g"] 
+
+for ord in list1:
+    list2.append(len(ord)) 
+
+print(list2)
+"""
+list1 = ["He", "P", "di"] 
+list2 = ["J","å", "g"]
+list3 = [] 
+
+for i in range(len(list1)):
+    list3.append(list1[i] + list2[i]) 
+
+print(list3) 
+
+
+
+
+
+
+
+    
